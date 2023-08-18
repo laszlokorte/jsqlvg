ab4922dbb7880d8d5a50482145df4a9857b3cbbb
Laszlo Korte <9korte@informatik.uni-hamburg.de>
2023-08-18 02:17:45 +0200

    initial commit

---
+++ js/main.js
@@ -0,0 +1,342 @@
+const rootEl = document.querySelector('#app-root');
+const canvasEl = document.createElement('canvas');
+const svgEl = document.createElementNS("http://www.w3.org/2000/svg",'svg');
+const svgElWorld = document.createElementNS("http://www.w3.org/2000/svg",'svg');
+const errorEl = document.createElement('div');
+
+rootEl.classList.add('layout-zstack','full-size')
+canvasEl.classList.add('draw-target','layout-zstack-item')
+svgEl.classList.add('draw-target','layout-zstack-item')
+svgElWorld.classList.add('draw-target','layout-zstack-item','no-mouse')
+errorEl.classList.add('error-report','layout-zstack-item')
+
+svgEl.setAttribute('preserveAspectRatio', 'xMidYMid slice')
+svgElWorld.setAttribute('preserveAspectRatio', 'xMidYMid slice')
+
+rootEl.appendChild(canvasEl)
+rootEl.appendChild(svgEl)
+rootEl.appendChild(svgElWorld)
+rootEl.appendChild(errorEl)
+svgEl.style.zIndex = 888
+svgElWorld.style.zIndex = 888
+errorEl.style.zIndex = 999
+
+const svgPoint = svgEl.createSVGPoint()
+const svgPointWorld = svgElWorld.createSVGPoint()
+
+function eventXY(event) {
+	var ctm = svgEl.getScreenCTM();
+	svgPoint.x = event.clientX;
+	svgPoint.y = event.clientY;
+	let {x,y} = svgPoint.matrixTransform(ctm.inverse());
+
+	return {x,y}
+}
+
+
+function eventXYWorld(event) {
+	var ctm = svgElWorld.getScreenCTM();
+	svgPoint.x = event.clientX;
+	svgPoint.y = event.clientY;
+	let {x,y} = svgPoint.matrixTransform(ctm.inverse());
+
+	return {x,y}
+}
+
+
+let w=0,h=0
+function resize() {
+	w = window.innerWidth
+	h = window.innerHeight
+
+	canvasEl.width = w*2
+	canvasEl.height = h*2
+	svgEl.setAttribute('width', w)
+	svgEl.setAttribute('height', h)
+}
+
+resize()
+window.addEventListener('resize', resize)
+window.onerror = (msg, url, lineNo, columnNo, error) => {
+	errorEl.innerText = `Error: ${lineNo}:${columnNo} in ${url}: ${msg}`
+}
+
+function fetchOne(db, sql) {
+	const stmt = db.prepare(sql);
+	stmt.step()
+	const result = stmt.getAsObject()
+	stmt.free()
+	return result;
+}
+
+function fetchAll(db, sql) {
+	const stmt = db.prepare(sql);
+	const results = []
+	while(stmt.step())
+		results.push(stmt.getAsObject())
+	stmt.free()
+
+	return results;
+}
+
+
+function render(db) {
+	const selected = fetchAll(db, "SELECT element_id FROM ui_selection  WHERE ui_viewport_id=1").map((e)=>e.element_id)
+	console.log(selected)
+	const points = fetchAll(db, "SELECT id, x, y, size, color FROM test")
+	const nodes = fetchAll(db, "SELECT * FROM view_bounded_node_in_viewport WHERE viewport_id=1 AND in_viewport");
+	const anchors = fetchAll(db, "SELECT * FROM view_bounded_anchor_in_viewport WHERE viewport_id=1 AND in_viewport");
+	const texts = fetchAll(db, "SELECT * FROM view_bounded_text_in_viewport WHERE viewport_id=1 AND in_viewport");
+	const edges = fetchAll(db, "SELECT * FROM view_bounded_edge_in_viewport WHERE viewport_id=1 AND in_viewport");
+	const selectBoxs = fetchAll(db, "SELECT * FROM ui_selection_box WHERE ui_viewport_id=1");
+	const viewport = fetchOne(db, "SELECT * FROM view_bounded_ui_viewport WHERE id=1");
+	svgEl.setAttribute('viewBox', `${viewport.min_x} ${viewport.min_y} ${viewport.width} ${viewport.height}`);
+	svgElWorld.setAttribute('viewBox', `${viewport.world_min_x} ${viewport.world_min_y} ${viewport.world_width} ${viewport.world_height}`);
+	const scaleFactor = Math.max(canvasEl.width/viewport.width, canvasEl.height/viewport.height)
+	const ctx = canvasEl.getContext("2d")	
+	ctx.clearRect(0,0, canvasEl.width, canvasEl.height)
+	ctx.save()
+	ctx.translate(canvasEl.width/2, canvasEl.height/2)
+	ctx.scale(scaleFactor, scaleFactor)
+
+	ctx.fillStyle = '#feea'
+	ctx.strokeStyle = '#faa'
+	ctx.lineWidth=1
+	ctx.fillRect(viewport.min_x, viewport.min_y, viewport.width, viewport.height)
+	ctx.strokeRect(viewport.min_x, viewport.min_y, viewport.width, viewport.height)
+
+	ctx.fillStyle = 'gray'
+	ctx.strokeStyle = 'black'
+	ctx.lineWidth = Math.min(1,viewport.scale)
+	
+	ctx.beginPath()
+	for(let node of nodes) {
+		ctx.rect(node.min_x, node.min_y, node.width, node.height)
+	}
+	ctx.fill()
+	ctx.stroke()
+
+	ctx.strokeStyle = 'blue'
+	ctx.beginPath()
+	ctx.lineWidth = 0.3
+	for(let edge of edges) {
+		//ctx.rect(edge.min_x, edge.min_y, edge.max_x-edge.min_x, edge.max_y-edge.min_y)
+
+		ctx.moveTo(edge.source_x, edge.source_y)
+		for(let p of JSON.parse(edge.points)) {
+			ctx.lineTo(p.x, p.y)
+			//ctx.fillText(edge.element_id, p.x, p.y)
+		}
+		ctx.lineTo(edge.target_x, edge.target_y)
+	}
+	ctx.stroke();
+
+	ctx.fillStyle = 'black'
+	ctx.textAlign="center"
+	ctx.textBaseline="middle"
+
+	for(let text of texts) {
+		ctx.font = `normal ${text.font_size_relative}px serif`;
+		ctx.fillText(text.content, text.center_x, text.center_y)
+	}
+
+
+	ctx.fillStyle = '#0af2'
+	ctx.strokeStyle = '#07da'
+
+	ctx.beginPath()
+	for(let selectBox of selectBoxs) {
+		ctx.rect(selectBox.start_x, selectBox.start_y, selectBox.end_x-selectBox.start_x, selectBox.end_y-selectBox.start_y)
+	}
+	ctx.fill()
+	ctx.stroke()
+
+	ctx.strokeStyle = '#07da'
+	ctx.lineWidth=3
+	ctx.lineJoin="round"
+	ctx.lineCap="round"
+
+	ctx.beginPath()
+	for(let node of nodes) {
+		if(!selected.includes(node.element_id)) {
+			continue
+		}
+		ctx.rect(node.min_x, node.min_y, node.width, node.height)
+	}
+	for(let edge of edges) {
+		if(!selected.includes(edge.element_id)) {
+			continue
+		}
+
+		ctx.moveTo(edge.source_x, edge.source_y)
+		for(let p of JSON.parse(edge.points)) {
+			ctx.lineTo(p.x, p.y)
+		}
+		ctx.lineTo(edge.target_x, edge.target_y)
+	}
+	ctx.stroke()
+	for(let text of texts) {
+		if(!selected.includes(text.element_id)) {
+			continue
+		}
+		ctx.font = `normal ${text.font_size_relative}px serif`;
+		ctx.strokeText(text.content, text.center_x, text.center_y)
+	}
+
+
+	ctx.fillStyle = 'orange'
+	ctx.strokeStyle = 'darkorange'
+	ctx.lineWidth = Math.min(1,viewport.scale)
+
+	ctx.beginPath()
+	for(let anchor of anchors) {
+		ctx.roundRect(anchor.min_x, anchor.min_y, anchor.width, anchor.height, 5*viewport.scale)
+	}
+	ctx.fill()
+	ctx.stroke()
+
+	ctx.restore()
+}
+
+function loadScript(path) {
+	return new Promise((resolve, reject) => {
+		const opScript = document.createElement('script')
+		opScript.async = true
+		opScript.onload = () => resolve()
+		opScript.onerror = () => reject()
+		opScript.src = path
+		document.head.appendChild(opScript)
+	})
+}
+
+function loadText(path) {
+	return window.fetch(path).then(s=>s.text())
+}
+
+Promise.all([initSqlJs({}), loadText('schema/main.sql'), loadScript('schema/operations.js')]).then(function([SQL, schema]){
+	const db = new SQL.Database();
+	const stmt = db.exec(schema);
+
+	loadExamples(db)
+
+	function doInsert(x,y,size,color) {
+		createNode(db, x,y, 1)
+		requestReload()
+	}
+
+
+	let reloading
+	function requestReload() {
+		if(!reloading){
+			reloading = window.requestAnimationFrame(doReload)
+		}
+	}
+
+	function doReload() {
+		reloading = null
+		render(db)
+	}
+
+	requestReload();
+
+	window.addEventListener('resize', requestReload);
+
+	;(() => {
+		let dragging = null
+		let dragged = 0
+
+		function startDragging(evt) {
+			dragged = 0
+			const xy = eventXY(evt)
+			dragging = {
+				start: xy,
+				prev: xy,
+				button: evt.button,
+			}
+
+			if(evt.button==0) {
+				startSelect(db, xy.x, xy.y)
+				requestReload()
+			}
+
+			moveDragging(evt)
+			evt.preventDefault()
+		}
+
+		function stopDragging(evt) {
+			if(dragging&&dragging.button==0) {
+				stopSelect(db)
+				requestReload()
+			}
+			dragging = null
+		}
+
+		function moveDragging(evt) {
+			if(!dragging) {
+				return
+			}
+			if(dragging.button==1 && dragged++ > 2) {
+				const {x,y} = eventXY(evt)
+				doPan(db, x-dragging.prev.x, y-dragging.prev.y)
+
+				dragging.prev = {x,y}
+				requestReload()
+			} else if(dragging.button==0 && dragged++ > 2) {
+				const {x,y} = eventXY(evt)
+
+				updateSelect(db, x, y)
+
+				dragging.prev = {x,y}
+				requestReload()
+			}
+		}
+
+		function click(evt) {
+			if(dragged > 2) {
+				return
+			}
+			const {x,y} = eventXYWorld(evt)
+
+			doInsert(x, y, 5, "#4af")
+		}
+
+		function zoom(evt) {
+			const center = eventXY(evt)
+			doZoom(db, Math.pow(2, -Math.sign(evt.deltaY)/4), center)
+			requestReload(db)
+		}
+
+		svgEl.addEventListener("click", click, false);
+		svgEl.addEventListener("dblclick", click, false);
+		svgEl.addEventListener("pointerdown", startDragging, false);
+		window.addEventListener("pointerup", stopDragging, false);
+		window.addEventListener("pointercancel", stopDragging, false);
+		window.addEventListener("pointermove", moveDragging, false);
+		svgEl.addEventListener("wheel", zoom, false);
+
+		const menu = document.createElement('menu');
+		const menuItems = [
+			{label: 'Clear', action: () => {
+				clearElements(db)
+				requestReload()
+			}},
+			{label: 'Delete Selected', action: () => {
+				deleteSelected(db)
+				requestReload()
+			}}
+		]
+
+		for(let item of menuItems) {
+			const menuItemClear = document.createElement('li')
+			const menuButtonClear = document.createElement('button')
+			menuItemClear.appendChild(menuButtonClear)
+			menu.appendChild(menuItemClear)
+			rootEl.appendChild(menu)
+			menu.classList.add('menu', 'layout-zstack-item')
+			menuButtonClear.classList.add('menu-button')
+			menuButtonClear.appendChild(document.createTextNode(item.label))
+			menuButtonClear.addEventListener('click', item.action)
+		}
+	})()
+})
+
---
+++ schema/operations.js
@@ -0,0 +1,217 @@
+function createShape(db) {
+	return db.exec(`INSERT INTO shape DEFAULT VALUES RETURNING ID`)[0].values[0][0];
+}
+
+function createAnchor(db, shapeId, relX = 0.5, relY = 0.5) {
+	const stmt = db.exec(`INSERT INTO 
+		shape_anchor (shape_id, relative_x, relative_y) 
+		VALUES (:shape_id, :relative_x, :relative_y) RETURNING ID`, 
+	{
+		':shape_id': shapeId,
+		':relative_x': relX,
+		':relative_y': relY,
+	});
+
+
+	return stmt[0].values[0][0];
+}
+
+
+function createNode(db, x,y, shapeId) {
+	const elementId = db.exec(`INSERT INTO element DEFAULT VALUES RETURNING ID`)[0].values[0][0];
+	const stmt = db.exec(`INSERT INTO node
+	(element_id, shape_id, position_x, position_y) VALUES 
+	(:element_id, :shape_id, :position_x, :position_y)
+	 RETURNING ID`, {
+		':element_id': elementId,
+		':shape_id': shapeId,
+		':position_x': x,
+		':position_y': y,
+	});
+
+	return [stmt[0].values[0][0], elementId];
+}
+
+function clearElements(db) {
+	db.exec(`DELETE FROM element`);
+}
+
+function deleteSelected(db) {
+	db.exec(`DELETE FROM element WHERE id IN (SELECT element_id FROM ui_selection WHERE ui_viewport_id = 1)`);
+}
+
+function createEdge(db, sourceNode, sourceAnchor, targetNode, targetAnchor, elementId = null) {
+	db.exec('BEGIN TRANSACTION');
+	elementId = elementId != null ? elementId : db.exec(`INSERT INTO element DEFAULT VALUES RETURNING ID`)[0].values[0][0];
+	const stmt = db.exec(`INSERT INTO edge
+	(element_id, 
+		source_node_id,
+		source_shape_id,
+		source_anchor_id,
+		target_node_id,
+		target_shape_id,
+		target_anchor_id) 
+			SELECT 
+			:element_id, source_node.id, source_anchor.shape_id, source_anchor.id,
+			target_node.id, target_anchor.shape_id, target_anchor.id 
+			FROM 
+			node source_node, 
+			node target_node
+			INNER JOIN shape_anchor source_anchor
+			ON source_anchor.shape_id = source_node.shape_id
+			INNER JOIN shape_anchor target_anchor
+			ON target_anchor.shape_id = target_node.shape_id
+			WHERE (source_node.id, target_node.id) = (:source_node_id, :target_node_id)
+			AND (source_anchor.id, target_anchor.id) = (:source_anchor_id, :target_anchor_id)
+			LIMIT 1
+	 RETURNING ID`, {
+		':element_id': elementId,
+		':source_node_id': sourceNode,
+		':source_anchor_id': sourceAnchor,
+		':target_node_id': targetNode,
+		':target_anchor_id': targetAnchor,
+	});
+
+	edgeId = stmt[0].values[0][0];
+	if (!edgeId) {
+		db.exec('ROLLBACK');
+		throw new Exception(`Creating Edge failed`);
+	} else { 
+	    db.exec('COMMIT');
+	}
+
+	return [edgeId, elementId];
+}
+
+function createPathPoint(db, elementId, x,y) {
+	const stmt = db.exec(`INSERT INTO path_point(element_id, x,y, sort)
+		VALUES(:element_id, :x, :y, 0)
+	 RETURNING ID`, {
+		':element_id': elementId,
+		':x': x,
+		':y': y,
+	});
+
+	return [stmt[0].values[0][0], elementId];
+}
+
+
+function createText(db, elementId, content = '', x = 0, y = 0) {
+
+	db.exec('BEGIN TRANSACTION');
+
+	const stmt = db.exec(`INSERT INTO text
+	(element_id, content, position_x, position_y) VALUES 
+	(:element_id, :content, :position_x, :position_y)
+	 RETURNING ID`, {
+		':element_id': elementId,
+		':content': content,
+		':position_x': x,
+		':position_y': y,
+	});
+
+	textId = stmt[0].values[0][0];
+
+	if (!textId) {
+		db.exec('ROLLBACK');
+		throw new Exception(`Creating Edge failed`);
+	} else { 
+	    db.exec('COMMIT');
+	}
+
+	return textId;
+}
+
+function repair(db) {
+	db.exec(`DELETE FROM element WHERE id IN (SELECT element_id FROM view_error_orphan_element)`);
+	db.exec(`DELETE FROM text WHERE id IN (SELECT text_id FROM view_error_empty_text)`);
+	db.exec(`DELETE FROM edge WHERE element_id IN (SELECT element_id FROM view_error_edge_node_conflict)`);
+}
+
+function doZoom(db, factor, center, min = 0.1, max= 10) {
+	db.exec(`UPDATE ui_camera 
+		SET zoom=MIN(MAX(:min, zoom*:factor), :max), 
+		center_x=center_x+(:x/zoom)*(1-1/(MIN(MAX(:min, zoom*:factor), :max)/zoom)), 
+		center_y=center_y+(:y/zoom)*(1-1/(MIN(MAX(:min, zoom*:factor), :max)/zoom)) 
+		WHERE id = 1`,{':factor':factor, ':x': center.x, ':y': center.y, ':min':min, ':max':max})
+}
+
+function doPan(db, dx, dy) {
+	db.exec(`UPDATE ui_camera 
+		SET
+		center_x=center_x-(:dx/zoom), 
+		center_y=center_y-(:dy/zoom)
+		WHERE id = 1`,{':dx':dx,':dy':dy})
+}
+
+function startSelect(db,x,y) {
+	db.exec(`DELETE FROM ui_selection_box`)
+	db.exec(`DELETE FROM ui_selection`)
+	db.exec(`INSERT INTO ui_selection_box(ui_viewport_id, start_x, start_y, end_x, end_y) 
+		VALUES(:vp, :x, :y,:x,:y)`,
+		{':vp':1, ':x':x,':y':y})
+}
+
+function updateSelect(db,x,y) {
+	db.exec(`UPDATE ui_selection_box SET end_x = :x, end_y = :y`,
+		{':vp':1, ':x':x,':y':y})
+	applySelect(db)
+}
+
+function stopSelect(db,x,y) {
+	db.exec(`DELETE FROM ui_selection_box`)
+}
+
+function applySelect(db,x,y) {
+	db.exec(`DELETE FROM ui_selection`)
+
+	db.exec(`INSERT OR IGNORE INTO ui_selection (ui_viewport_id, element_id) 
+		SELECT ui_viewport_id, element_id FROM view_elements_in_select_box
+		`)
+}
+
+function loadExamples(db) {
+
+	shapeId1 = createShape(db);
+	shapeId2 = createShape(db);
+	shapeId3 = createShape(db);
+
+	anchor1 = createAnchor(db, shapeId1);
+	anchor2 = createAnchor(db, shapeId1,0,0);
+	anchor3 = createAnchor(db, shapeId1,0,1);
+	anchor4 = createAnchor(db, shapeId1,1,0);
+	anchor5 = createAnchor(db, shapeId1,1,1);
+
+	n3 = createNode(db, 0,-30, shapeId1);
+	n1 = createNode(db, 20,40, shapeId1);
+	n2 = createNode(db, -50,80, shapeId1);
+
+	e1 = createEdge(db, n1[0], anchor3, n2[0], anchor5);
+	e2 = createEdge(db, n2[0], anchor2, n3[0], anchor2);
+	createPathPoint(db, e2[1], -90, 0);
+	createPathPoint(db, e1[1], 10, 100);
+
+	t1 = createText(db, n1[1], `foo`,0,20);
+	t2 = createText(db, n2[1], `foo`,0,-10);
+	t3 = createText(db, e2[1], `foo`);
+
+	t3 = createText(db, e2[1], ``);
+
+	console.log(db)
+	db.exec(`INSERT INTO element DEFAULT VALUES RETURNING ID`)[0].values[0][0];
+	e3 = createEdge(db, n2[0], anchor2, n3[0], anchor2, n2[1]);
+
+	camId1 = db.exec(`INSERT INTO ui_camera(center_x,center_y,zoom) VALUES (
+		0,
+		0,
+		0.6) RETURNING ID`)[0].values[0][0];
+	camId2 = db.exec(`INSERT INTO ui_camera(center_x,center_y,zoom) VALUES (
+		ROUND(200*RANDOM()/9223372036854775807),
+		ROUND(200*RANDOM()/9223372036854775807),
+		0.6) RETURNING ID`)[0].values[0][0];
+	const vpStmt = db.prepare(`INSERT INTO ui_viewport(width,height,ui_camera_id) VALUES (:w,:h,:camera_id)  RETURNING ID`);
+	const vpId = vpStmt.getAsObject({':camera_id': camId1, ':w': 500, ':h': 300}).id
+	vpStmt.free()
+
+	repair(db)
+}
